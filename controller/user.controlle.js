9c438397bb852ef2a5d575a107d891462f96326e
karans18 <karannikumbh18@gmail.com>
2026-05-23 21:10:48 +0530

    login

--- controller/user.controlle.js
+++ controller/user.controlle.js
@@ -2,24 +2,25 @@ import cloudinary from "../config/cloudinary.js";
 import bcrypt from "bcrypt";
 import User from "../model/user.model.js";
 import mongoose from "mongoose";
-
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
+  const { email, password, channelName, phone } = req.body;
   try {
     console.log("request coming");
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
     console.log(hashedPassword);
     const uploadImage = await cloudinary.uploader.upload(
-      req.files.logo.tempFilePath,
+      req.files.logoUrl.tempFilePath,
     );
     console.log("IMAGE👉", uploadImage);
 
     const newUser = new User({
       _id: new mongoose.Types.ObjectId(),
-      email: req.body.email,
+      email: email,
       password: hashedPassword,
-      channelName: req.body.channelName,
-      phone: req.body.phone,
+      channelName: channelName,
+      phone: phone,
       logoUrl: uploadImage.secure_url,
       logoId: uploadImage.public_id,
     });
@@ -36,3 +37,49 @@ export const signup = async (req, res) => {
       .json({ error: "something went wrong", message: error.message });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(401).json({ message: "User Not Found" });
+    }
+
+    const isValid = await bcrypt.compare(password, existingUser.password);
+    if (!isValid) {
+      return res.status(401).json({ message: "invalid Credentials" });
+    }
+    const token = jwt.sign(
+      {
+        _id: existingUser._id,
+        email: existingUser.email,
+        channelName: existingUser.channelName,
+        phone: existingUser.phone,
+        logoId: existingUser.logoId,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "10d" },
+    );
+
+    res.status(200).json({
+      _id: existingUser._id,
+      email: existingUser.email,
+      channelName: existingUser.channelName,
+      phone: existingUser.phone,
+      logoId: existingUser.logoId,
+      logoUrl: existingUser.logoUrl,
+      token: token,
+      subscribedChannels: existingUser.subscribedChannels,
+      subscribers: existingUser.subscribers,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "something went wrong", message: error.message });
+  }
+};
+
+/1:14
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,33 +1,11 @@
 import express from "express";
-import { signup } from "../controller/user.controlle.js";
+import { login, signup } from "../controller/user.controlle.js";
 // import multer from "multer";
 // import fs from "fs";
 
 const router = express.Router();
-// const uploadDir = "uploads";
-
-// fs.mkdirSync(uploadDir, { recursive: true });
-
-// // Configure multer — use diskStorage or memoryStorage
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => cb(null, uploadDir),
-//   filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
-// });
-
-// const upload = multer({ storage });
-
-// const uploadLogo = (req, res, next) => {
-//   upload.single("logo")(req, res, (err) => {
-//     if (!err) return next();
-
-//     return res.status(400).json({
-//       message:
-//         "Invalid multipart form data. Send the logo field as a file using form-data and let your client set Content-Type automatically.",
-//       error: err.message,
-//     });
-//   });
-// };
 
 router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
